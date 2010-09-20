08f58b5e2519eca6685ccc475469efd6c747b96c
Janos Gyerik <janos@wall-e>
2010-09-21 08:09:16 +0900

    added rule for serving static media in debug mode

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^musiclibrary/', include('musiclibrary.foo.urls')),
-    (r'^folders/', include('musiclibrary.folders.urls')),
+    (r'^folder/', include('musiclibrary.folder.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
@@ -19,3 +19,11 @@ urlpatterns = patterns('',
 
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
+
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+	    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'media'}),
+    )
+
