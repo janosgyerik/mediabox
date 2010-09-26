870a1308a1a646d1100383b1677341c30ed59cbe
Janos Gyerik <janos@wall-e>
2010-09-26 11:02:13 +0900

    added password reset function

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,10 @@ urlpatterns = patterns('django.contrib.auth.views',
     (r'^logout_then_login/$', 'logout_then_login'),
     (r'^password_change/$', 'password_change'),
     (r'^password_change_done/$', 'password_change_done'),
+    (r'^password_reset/$', 'password_reset'),
+    (r'^password_reset_done/$', 'password_reset_done'),
+    (r'^password_reset_confirm/(?P<uidb36>.*)/(?P<token>.*)$', 'password_reset_confirm'),
+    (r'^password_reset_complete/$', 'password_reset_complete'),
 )
 
 # eof
