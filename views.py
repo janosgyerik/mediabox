74136877a261a468f819078eb625d0f0ab75432d
Janos Gyerik <janos@axiom>
2012-06-20 22:59:54 +0200

    simplified home screen

--- views.py
+++ views.py
@@ -1,17 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.conf import settings
 
-from django.contrib.auth.decorators import login_required
-
-from folders.views import folders, latest_x
-
-#@login_required
 def home(request):
-    return render_to_response('home.html', 
-            { 
-                "folders": folders(), 
-                "latest": latest_x(10),
-                }, RequestContext(request))
+    return render_to_response('home.html', RequestContext(request))
 
 # eof
