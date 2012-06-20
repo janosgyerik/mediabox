b88a2d7a33d36939fe7fbe0c9364c0cfc8e00b5d
Janos Gyerik <janos@axiom>
2012-06-20 23:31:57 +0200

    reindent

--- folders/views.py
+++ folders/views.py
@@ -13,23 +13,21 @@ MUSIC_ROOT = settings.MUSIC_ROOT
 #@login_required
 def index(request, relpath=None):
     if relpath is None:
-	return render_to_response('folders/index.html',
-		{
-		    "foldername": "Music Library",
-		    "folders": folders(relpath),
-		    }, RequestContext(request))
+	return render_to_response('folders/index.html', {
+        "foldername": "Music Library",
+        "folders": folders(relpath),
+        }, RequestContext(request))
     else:
-	return render_to_response('folders/index.html',
-		{
-		    "foldername": os.path.basename(relpath),
-		    "folders": folders(relpath),
-		    "files": files(relpath),
-		    "locations": locations(relpath),
-		    }, RequestContext(request))
+        return render_to_response('folders/index.html', {
+            "foldername": os.path.basename(relpath),
+            "folders": folders(relpath),
+            "files": files(relpath),
+            "locations": locations(relpath),
+            }, RequestContext(request))
 
 def locations(relpath=None):
     if relpath is None:
-	return
+        return
 
     import django.core.urlresolvers
     href_base = django.core.urlresolvers.reverse("folders")
@@ -37,24 +35,24 @@ def locations(relpath=None):
     locations = []
     head = relpath
     while True:
-	(head, tail) = os.path.split(head)
-	href = os.path.join(href_base, head, tail)
+        (head, tail) = os.path.split(head)
+        href = os.path.join(href_base, head, tail)
 
-	item = {
-		"name": tail,
-		"href": href,
-		}
+        item = {
+                "name": tail,
+                "href": href,
+                }
 
-	locations.append(item)
+        locations.append(item)
 
-	if head == "":
-	    break
+        if head == "":
+            break
 
     locations[0]["last"] = True
     locations.append({
-	"name": "Top",
-	"href": href_base,
-    })
+        "name": "Top",
+        "href": href_base,
+        })
     locations.reverse()
 
     return locations
@@ -73,18 +71,18 @@ def folders(relpath=None):
 
     folders = []
     for f in os.listdir(mediapath):
-	if os.path.isdir(os.path.join(mediapath, f)):
-	    if relpath is None:
-		href = os.path.join(href_base, f)
-	    else:
-		href = os.path.join(href_base, relpath, f)
+        if os.path.isdir(os.path.join(mediapath, f)):
+            if relpath is None:
+                href = os.path.join(href_base, f)
+            else:
+                href = os.path.join(href_base, relpath, f)
 
-	    folder = {
-		    "name": f,
-		    "href": href,
-		    }
+            folder = {
+                    "name": f,
+                    "href": href,
+                    }
 
-	    folders.append(folder)
+            folders.append(folder)
 
     return folders
 
@@ -93,32 +91,30 @@ def files(relpath=None):
     href_base = "/media/mp3"
 
     if relpath is None:
-	mediapath = MUSIC_ROOT
+        mediapath = MUSIC_ROOT
     else:
-	mediapath = os.path.join(MUSIC_ROOT, relpath)
+        mediapath = os.path.join(MUSIC_ROOT, relpath)
 
     if not os.path.isdir(mediapath):
-	return
+        return
 
     files = []
     for f in os.listdir(mediapath):
-	if os.path.isfile(os.path.join(mediapath, f)):
-	    if relpath is None:
-		href = os.path.join(href_base, f)
-	    else:
-		href = os.path.join(href_base, relpath, f)
+        if os.path.isfile(os.path.join(mediapath, f)):
+            if relpath is None:
+                href = os.path.join(href_base, f)
+            else:
+                href = os.path.join(href_base, relpath, f)
 
-	    file = {
-		    "name": f,
-		    "href": href,
-		    "size": os.path.getsize(os.path.join(mediapath, f)),
-		    }
+            file = {
+                    "name": f,
+                    "href": href,
+                    "size": os.path.getsize(os.path.join(mediapath, f)),
+                    }
 
-	    files.append(file)
+            files.append(file)
 
     return files
 
-def latest_x(num=0):
-    pass
 
 # eof
