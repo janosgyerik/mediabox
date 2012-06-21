8a070534284cc34849379adc9320edcae5eec65e
Janos Gyerik <janos@xw8400.home>
2012-06-21 20:48:55 +0200

    added proper models

--- folders/models.py
+++ folders/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre:
+    name = models.CharField(max_length=80)
+
+
+class Artist:
+    name = models.CharField(max_length=80)
+    genre = models.ForeignKey(Genre)
+
+
+class Album:
+    title = models.CharField(max_length=80)
+    artist = models.ForeignKey(Artist)
+    release_date = models.DateTimeField()
+    num_tracks = models.IntegerField(help='Number of tracks')
+
+
+class AlbumSong:
+    title = models.CharField(max_length=200)
+    album = models.ForeignKey(Album)
+    track = models.IntegerField()
+
+
+class Single:
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist)
+    release_date = models.DateTimeField()
+
+
+# eof
