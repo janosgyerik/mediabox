fba73ba249c05204f4590162dbc342278fd43be4
Janos Gyerik <janos@xw8400.home>
2012-06-21 21:49:07 +0200

    progress with mp3 import

--- folders/utils.py
+++ folders/utils.py
@@ -1,18 +1,16 @@
 import os
+from datetime import datetime
 from mutagen.easyid3 import EasyID3
 
 from django.conf import settings
 
-#from folders.models import Artist, Album, AlbumSong
+from folders.models import Genre, Artist, Album, AlbumSong
 
 
 def sync_music():
     for abspath, relpath in find_music_files():
         audioinfo = get_audioinfo(abspath)
-        audioinfo.pprint()
-        print audioinfo.keys()
-        print audioinfo['title']
-        print audioinfo['artist']
+        genre = get_genre_by_name(audioinfo['genre'])
         break
 
 
@@ -30,9 +28,27 @@ def find_music_files():
 def get_audioinfo(filepath):
     audioinfo = None
     if filepath.endswith('.mp3'):
-        audioinfo = EasyID3(filepath)
+        audioinfo = get_audioinfo_by_easyid3(EasyID3(filepath))
 
     return audioinfo
+
+
+def get_audioinfo_by_easyid3(easyid3):
+    (track, num_tracks) = easyid3['tracknumber'][0].split('/')
+    released_date = datetime(int(easyid3['date'][0]), 1, 1)
+    return {
+            'artist': easyid3['artist'][0],
+            'genre': easyid3['genre'][0],
+            'album': easyid3['album'][0],
+            'title': easyid3['title'][0],
+            'track': track,
+            'num_tracks': num_tracks,
+            'released_date': released_date,
+            }
+
+
+def get_genre_by_name(name):
+    return Genre.objects.get(name=name)
 
 
 # eof
